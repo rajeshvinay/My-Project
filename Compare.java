2293c22db16abb7cbb3a63f2f69a3d37319771a8
rajeshvinay <30381421+rajeshvinay@users.noreply.github.com>
2017-07-23 11:52:38 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,38 @@
+class Big
+{       
+static private int p;
+static protected int q;
+static int r; 
+Compare(int x,int y, int z)
+	{
+	p=x;
+	q=y;
+	r=z;
+	Compare.getNum();
+	Compare.findBigNumber(p,q,r);
+	}
+static void getNum()
+	{
+	System.out.println(p);
+	System.out.println(q);
+	System.out.println(r);
+	}
+static void findBigNumber(int a,int b,int c)
+	{	
+	if ( a>b && a>c )
+         System.out.println("First number is largest.");
+      else if ( b>a && b>c )
+         System.out.println("Second number is largest.");
+      else if ( c>a && c>b )
+         System.out.println("Third number is largest.");
+      else   
+         System.out.println("The numbers are not distinct.");	
+	}
+}
+ class Compare
+{
+	public static void main(String args[])
+	{
+	 Compare g = new Compare(25,65,76);
+	}
+}
